ae9de7c430244017ae0ba1ae54699610d5ca615b
MohammadReza Bagherian <95492218+realBagher@users.noreply.github.com>
2024-10-19 17:24:17 +0330

    order model created!

--- torobEcom/Orders/models.py
+++ torobEcom/Orders/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from Customers.models import Customer
 
-# Create your models here.
+
+class Order(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, related_name="orders"
+    )
+    product_name = models.CharField(max_length=255)
+    order_status = models.CharField(
+        max_length=50,
+        choices=[
+            ("Pending", "Pending"),
+            ("Shipped", "Shipped"),
+            ("Delivered", "Delivered"),
+        ],
+    )
+    order_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order {self.id} for {self.product_name}"
