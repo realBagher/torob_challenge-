b338e38835159738cf7506cda160f4f665213590
MohammadReza Bagherian <95492218+realBagher@users.noreply.github.com>
2024-10-19 15:24:39 +0330

    templates setting configurations

--- torobEcom/torobEcom/settings.py
+++ torobEcom/torobEcom/settings.py
@@ -62,10 +62,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "torobEcom.urls"
 
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
