732a75577fd6a9f563a3506864b749e363f5159d
MohammadReza Bagherian <95492218+realBagher@users.noreply.github.com>
2024-10-19 16:23:14 +0330

    customer model created

--- torobEcom/Customers/models.py
+++ torobEcom/Customers/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Customer(AbstractUser):
+    phone_number = models.CharField(max_length=15, unique=True)
+    is_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
