89dd72a4ce318ff83c830674a9d1f813c7cb4724
MohammadReza Bagherian <95492218+realBagher@users.noreply.github.com>
2024-10-20 18:27:20 +0330

    sign in sign up and registeration functionality

--- torobEcom/templates/core/urls.py
+++ torobEcom/templates/core/urls.py
@@ -7,7 +7,6 @@ app_name = "Core"  # This sets the namespace for the Core app
 # Views for different templates like index, partials, etc.
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html"), name="index"),
-    path("logout", TemplateView.as_view(template_name="index.html"), name="logout"),
-    path("signin", TemplateView.as_view(template_name="sign-in.html"), name="sign-in"),
-    path("signup", TemplateView.as_view(template_name="sing-up.html"), name="sign-up"),
+    path("", TemplateView.as_view(template_name="index.html"), name="logout"),
+    path("", TemplateView.as_view(template_name="index.html"), name="sign-in"),
 ]
