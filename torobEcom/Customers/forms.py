dc6731ee9817b9a9bdeb641355347482a9ae7c66
MohammadReza Bagherian <95492218+realBagher@users.noreply.github.com>
2024-10-20 12:49:33 +0330

    partial css and media satic

--- torobEcom/Customers/forms.py
+++ torobEcom/Customers/forms.py
@@ -1,15 +1,32 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from .models import Customer
+from .models import Customer, Address
 
 
-class CustomerRegistrationForm(UserCreationForm):
-    phone_number = forms.CharField(max_length=15)
+class CustomerRegistrationForm(forms.ModelForm):
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         model = Customer
-        fields = ["username", "phone_number", "password1", "password2"]
+        fields = ["username", "phone_number", "email", "password1", "password2"]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get("password1")
+        password2 = cleaned_data.get("password2")
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match.")
+        return cleaned_data
 
 
-class OTPVerificationForm(forms.Form):
-    otp = forms.CharField(max_length=6, required=True)
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput, required=False)
+    otp = forms.CharField(max_length=6, required=False)
+
+
+class AddressForm(forms.ModelForm):
+    class Meta:
+        model = Address
+        fields = ["street", "city", "postal_code", "country"]
