38c2e8a71e086ff8c6934406283e3a2fa48785f8
MohammadReza Bagherian <95492218+realBagher@users.noreply.github.com>
2024-10-25 10:49:27 +0330

    signup and registeration urls and integreation with template

--- torobEcom/Core/urls.py
+++ torobEcom/Core/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html"), name="index"),
     path("logout", TemplateView.as_view(template_name="index.html"), name="logout"),
     path("signin", TemplateView.as_view(template_name="sign-in.html"), name="sign-in"),
-    path("signup", TemplateView.as_view(template_name="sing-up.html"), name="sign-up"),
+    path("signup", TemplateView.as_view(template_name="sign-up.html"), name="sign-up"),
 ]
