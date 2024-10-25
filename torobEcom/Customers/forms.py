f48f51f271e08c3e47fb3a9514f1f0a1e86e986a
MohammadReza Bagherian <95492218+realBagher@users.noreply.github.com>
2024-10-25 11:04:49 +0330

    edit customer:registeration form

--- torobEcom/Customers/forms.py
+++ torobEcom/Customers/forms.py
@@ -1,23 +1,47 @@
 from django import forms
 from .models import Customer, Address
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import CustomerRegistrationForm, AddressFormSet
 
 
-class CustomerRegistrationForm(forms.ModelForm):
-    password1 = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(widget=forms.PasswordInput)
+class CustomerRegistrationView(CreateView):
+    model = Customer
+    form_class = CustomerRegistrationForm
+    template_name = "customers/register.html"
+    success_url = reverse_lazy("home")
 
-    class Meta:
-        model = Customer
-        fields = ["username", "phone_number", "email", "password1", "password2"]
+    def get(self, request, *args, **kwargs):
+        # Initialize customer registration form and address formset
+        customer_form = self.form_class()
+        address_formset = AddressFormSet()
+        return render(
+            request,
+            self.template_name,
+            {"customer_form": customer_form, "address_formset": address_formset},
+        )
 
-    def clean(self):
-        cleaned_data = super().clean()
-        password1 = cleaned_data.get("password1")
-        password2 = cleaned_data.get("password2")
+    def post(self, request, *args, **kwargs):
+        customer_form = self.form_class(request.POST)
+        address_formset = AddressFormSet(request.POST)
 
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match.")
-        return cleaned_data
+        if customer_form.is_valid() and address_formset.is_valid():
+            customer = customer_form.save(commit=False)
+            customer.set_password(
+                customer_form.cleaned_data["password1"]
+            )  # Set the password
+            customer.save()
+
+            # Save addresses
+            address_formset.instance = customer
+            address_formset.save()
+
+            login(request, customer)  # Log the user in after successful registration
+            return redirect(self.success_url)
+        else:
+            return
 
 
 class LoginForm(forms.Form):
