63959fbb9a27a14aed15d82b6e7f637505ed059d
MohammadReza Bagherian <95492218+realBagher@users.noreply.github.com>
2024-10-26 16:47:51 +0330

    feat: emails_utils.py , creation of email utils functionality to send

--- torobEcom/Core/emails_utils.py
+++ torobEcom/Core/emails_utils.py
@@ -7,7 +7,7 @@ def send_otp_email(user, otp_code):
     """
     subject = "Your OTP Code"
     message = f"Your OTP code is {otp_code}. It is valid for 5 minutes."
-    from_email = "noreply@myapp.com"
+    from_email = "myapp.test.host@gmail.com"
     recipient_list = [user.email]
 
     send_mail(subject, message, from_email, recipient_list, fail_silently=False)
