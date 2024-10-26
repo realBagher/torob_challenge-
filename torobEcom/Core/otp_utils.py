c1a6837a3c9072fdbc4cfbec0a9aee2b6b89deaa
MohammadReza Bagherian <95492218+realBagher@users.noreply.github.com>
2024-10-26 16:48:33 +0330

    feat: otp_utils , creating otp generation and verification and redis handeling

--- torobEcom/Core/otp_utils.py
+++ torobEcom/Core/otp_utils.py
@@ -2,13 +2,13 @@ import random
 from django_redis import get_redis_connection
 
 
-def generate_otp(user):
+def generate_otp(user_id):
     """
     Generate a 6-digit OTP for the user and store it in Redis with a 5-minute expiry.
     """
     otp_code = random.randint(100000, 999999)
     redis_conn = get_redis_connection("default")
-    redis_conn.set(f"otp:{user.id}", otp_code, ex=300)  # Store OTP for 5 minutes
+    redis_conn.set(f"otp:{user_id}", otp_code, ex=300)
     return otp_code
 
 
@@ -20,6 +20,6 @@ def verify_otp(user, otp):
     stored_otp = redis_conn.get(f"otp:{user.id}")
 
     if stored_otp and otp == stored_otp.decode("utf-8"):
-        redis_conn.delete(f"otp:{user.id}")  # OTP used, delete it
+        redis_conn.delete(f"otp:{user.id}")  # delete otp code
         return True
     return False
