596ffbdd7ecca93e02d706c6150cf8bacc9603d3
MohammadReza Bagherian <95492218+realBagher@users.noreply.github.com>
2024-10-31 17:48:44 +0330

    fix product models.py fix structure

--- torobEcom/Products/models.py
+++ torobEcom/Products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.exceptions import ValidationError
-from django.db import models
+from django.utils.text import slugify
 from django.utils.html import mark_safe
 from django.utils.deconstruct import deconstructible
 from PIL import Image
@@ -15,16 +15,16 @@ class ImageOrSVGValidator:
         if ext not in valid_extensions:
             raise ValidationError("Unsupported file extension.")
 
-        # Check file type if it's not SVG
         if ext != ".svg":
             try:
                 img = Image.open(file)
-                img.verify()  # This will throw an exception if it's not a valid image
+                img.verify()
             except (IOError, SyntaxError):
                 raise ValidationError("Unsupported image file or corrupted image.")
 
 
 class Category(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     parent_category = models.ForeignKey(
@@ -40,6 +40,12 @@ class Category(models.Model):
         blank=True,
         null=True,
     )
+    slug = models.SlugField(unique=True, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -49,14 +55,22 @@ class Category(models.Model):
 
 
 class Brand(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(Brand, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to="Product")
@@ -66,6 +80,12 @@ class Product(models.Model):
         Category, on_delete=models.CASCADE, related_name="products"
     )
     stock = models.PositiveIntegerField(default=0)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(Product, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -78,7 +98,7 @@ class Product(models.Model):
 
 
 class ProductImage(models.Model):
-
+    id = models.BigAutoField(primary_key=True)
     image = models.ImageField(upload_to="product-images")
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
 
@@ -92,6 +112,7 @@ class Discount(models.Model):
         ("fixed", "Fixed Amount"),
     ]
 
+    id = models.BigAutoField(primary_key=True)
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name="discounts"
     )
@@ -100,15 +121,28 @@ class Discount(models.Model):
         max_digits=5, decimal_places=2, help_text="Percentage or Fixed amount"
     )
     active = models.BooleanField(default=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(f"{self.discount_type}-{self.product.name}")
+        super(Discount, self).save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.discount_type} discount on {self.product.name}"
 
 
 class Gift(models.Model):
+    id = models.BigAutoField(primary_key=True)
     code = models.CharField(max_length=50, unique=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="gifts")
     active = models.BooleanField(default=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.code)
+        super(Gift, self).save(*args, **kwargs)
 
     def __str__(self):
         return f"Gift Code: {self.code} for {self.product.name}"
