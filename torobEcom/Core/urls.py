d91314bd5e6ea3384309d79581ef5c1680238ba7
MohammadReza Bagherian <95492218+realBagher@users.noreply.github.com>
2024-10-31 14:24:46 +0330

    fix core.url.py     - fix routes for index view

--- torobEcom/Core/urls.py
+++ torobEcom/Core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from Core.views import index_view
 from Customers.views import (
     CustomerRegistrationView,
     LoginView,
@@ -7,13 +8,13 @@ from Customers.views import (
     verify_otp_view,
     logout_view,
 )
+from Products.views import category_list
+
+app_name = "Core"  
 
 
-app_name = "Core"  # This sets the namespace for the Core app
-
-# Views for different templates like index, partials, etc.
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="index.html"), name="index"),
+    path("", index_view, name="index"),
     path("signup", CustomerRegistrationView.as_view(), name="sign-up"),
     path("login", LoginView.as_view(), name="login"),
     path("otp-login/", send_otp_view, name="otp-login"),
